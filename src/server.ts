2168fcb6149850c684f113c93c4bf930ef8e8e59
Stanly <stanlyalmeidabr@gmail.com>
2026-06-19 10:40:02 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
